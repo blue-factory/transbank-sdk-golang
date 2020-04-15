dae6b259f4eb1ac67f1d4d79abed9ff3046f0adc
camilo <camilo@MacBook-Pro-de-camilo.local>
2020-04-15 02:22:00 -0400

    add example to use webpay plus normal service on integration environment

---
+++ cmd/plus-normal/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+
+	"github.com/microapis/transbank-sdk-golang/pkg/configuration"
+	"github.com/microapis/transbank-sdk-golang/pkg/service"
+	"github.com/microapis/transbank-sdk-golang/pkg/webpay"
+)
+
+func main() {
+	amount := float64(1000)
+	sessionID := "mi-id-de-sesion"
+	buyOrder := int64(rand.Intn(9999999))
+	returnURL := "https://callback/resultado/de/transaccion"
+	finalURL := "https://callback/final/post/comprobante/webpay"
+
+	c := configuration.GetIntegrationPlusNormal()
+	w := webpay.New(c)
+	t := service.GetPlusNormal(w)
+
+	res, err := t.InitTransaction(service.ParamsPlusNormal{
+		Amount:    amount,
+		SessionID: sessionID,
+		BuyOrder:  buyOrder,
+		ReturnURL: returnURL,
+		FinalURL:  finalURL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("URL", res.URL)
+	log.Println("Token", res.Token)
+}
